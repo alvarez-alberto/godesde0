17b89ae296e0b6ecc71a74a7282971f0b8cbda94
alvarez-alberto <alberto.alvarez512@gmail.com>
2023-03-21 22:18:02 -0500

    se añade metodos if y switch

--- main.go
+++ main.go
@@ -2,13 +2,28 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/alvarez-alberto/godesde0/variables"
+	"runtime"
 )
 
 func main() {
-	estado, texto := variables.ConviertoaTexto(123)
+	/*
+		estado, texto := variables.ConviertoaTexto(123)
 
-	fmt.Println(estado)
-	fmt.Println(texto)
+		fmt.Println(estado)
+		fmt.Println(texto)
+	*/
+	if os := runtime.GOOS; os == "Linux." || os == "OS X." {
+		fmt.Println("No es windows")
+	} else {
+		fmt.Println("windows")
+	}
+
+	switch os := runtime.GOOS; os {
+	case "Linux.":
+		fmt.Println("Esto es Linux")
+	case "Darwin.":
+		fmt.Println("Esto es Darwin")
+	default:
+		fmt.Printf("%s \n", os)
+	}
 }
