a468a92de89da52db382d18e31ce853c92781853
alvarez-alberto <alberto.alvarez512@gmail.com>
2023-03-21 22:51:06 -0500

    se añade manejo de entrada de datos por terminal usando bufio

--- main.go
+++ main.go
@@ -1,35 +1,35 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/alvarez-alberto/godesde0/ejercicios"
-)
+import "github.com/alvarez-alberto/godesde0/teclado"
 
 func main() {
 	/*
-			estado, texto := variables.ConviertoaTexto(123)
+				estado, texto := variables.ConviertoaTexto(123)
 
-			fmt.Println(estado)
-			fmt.Println(texto)
+				fmt.Println(estado)
+				fmt.Println(texto)
 
-		if os := runtime.GOOS; os == "Linux." || os == "OS X." {
-			fmt.Println("No es windows")
-		} else {
-			fmt.Println("windows")
-		}
+			if os := runtime.GOOS; os == "Linux." || os == "OS X." {
+				fmt.Println("No es windows")
+			} else {
+				fmt.Println("windows")
+			}
 
-		switch os := runtime.GOOS; os {
-		case "Linux.":
-			fmt.Println("Esto es Linux")
-		case "Darwin.":
-			fmt.Println("Esto es Darwin")
-		default:
-			fmt.Printf("%s \n", os)
-		}
+			switch os := runtime.GOOS; os {
+			case "Linux.":
+				fmt.Println("Esto es Linux")
+			case "Darwin.":
+				fmt.Println("Esto es Darwin")
+			default:
+				fmt.Printf("%s \n", os)
+			}
+
+
+		numero, texto := ejercicios.ConvNumerico("ffff")
+		fmt.Println(numero)
+		fmt.Println(texto)
+
 	*/
 
-	numero, texto := ejercicios.ConvNumerico("ffff")
-	fmt.Println(numero)
-	fmt.Println(texto)
+	teclado.IngresoNumeros()
 }
