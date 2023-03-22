3a7451daadd8a36aaf491e1a79e17e64032550d7
alvarez-alberto <alberto.alvarez512@gmail.com>
2023-03-21 22:38:43 -0500

    primer ejercicio

--- main.go
+++ main.go
@@ -2,28 +2,34 @@ package main
 
 import (
 	"fmt"
-	"runtime"
+
+	"github.com/alvarez-alberto/godesde0/ejercicios"
 )
 
 func main() {
 	/*
-		estado, texto := variables.ConviertoaTexto(123)
+			estado, texto := variables.ConviertoaTexto(123)
 
-		fmt.Println(estado)
-		fmt.Println(texto)
+			fmt.Println(estado)
+			fmt.Println(texto)
+
+		if os := runtime.GOOS; os == "Linux." || os == "OS X." {
+			fmt.Println("No es windows")
+		} else {
+			fmt.Println("windows")
+		}
+
+		switch os := runtime.GOOS; os {
+		case "Linux.":
+			fmt.Println("Esto es Linux")
+		case "Darwin.":
+			fmt.Println("Esto es Darwin")
+		default:
+			fmt.Printf("%s \n", os)
+		}
 	*/
-	if os := runtime.GOOS; os == "Linux." || os == "OS X." {
-		fmt.Println("No es windows")
-	} else {
-		fmt.Println("windows")
-	}
 
-	switch os := runtime.GOOS; os {
-	case "Linux.":
-		fmt.Println("Esto es Linux")
-	case "Darwin.":
-		fmt.Println("Esto es Darwin")
-	default:
-		fmt.Printf("%s \n", os)
-	}
+	numero, texto := ejercicios.ConvNumerico("ffff")
+	fmt.Println(numero)
+	fmt.Println(texto)
 }
